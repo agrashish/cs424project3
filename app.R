15bbc63d6f807f6535a7c7573340f365591463f4
Aashish Agrawal <agrawalaashish1@gmail.com>
2020-04-26 18:02:07 -0500

    finished side by side plots

--- app.R
+++ app.R
@@ -221,13 +221,13 @@ ui <- dashboardPage(
             box( width = 12, status = "primary", solidHeader = TRUE, plotOutput("MovieKeywordsChart", height = 420)),
             ##table
             box( width = 12, status = "primary", solidHeader = TRUE, DT::dataTableOutput("MovieKeywordsTable", height = 360))
-          ),
-          tabPanel("Runtime of each Genre",      
-            ##chart
-            box( width = 12, status = "primary", solidHeader = TRUE, plotOutput("RuntimeGenrePlot", height = 360)),
-            ##table
-            box( width = 12, status = "primary", solidHeader = TRUE, DT::dataTableOutput("RuntimeGenreTable", height = 360))
-          )
+          )#,
+          # tabPanel("Runtime of each Genre",      
+          #   ##chart
+          #   box( width = 12, status = "primary", solidHeader = TRUE, plotOutput("RuntimeGenrePlot", height = 360)),
+          #   ##table
+          #   box( width = 12, status = "primary", solidHeader = TRUE, DT::dataTableOutput("RuntimeGenreTable", height = 360))
+          # )
         ) # end tabsetpanel
       ) # end mainpanel
     ) # end box
@@ -279,6 +279,16 @@ server <- function(input, output) {
     filteredKeywords <- keywords[1:input$pickFilter,]
     rownames(filteredKeywords) <- NULL
     filteredKeywords
+  })
+  
+  extraKeywords <- reactive({
+    #dataframe for keywords
+    keywords2 <- as.data.frame(table(data()$Keyword))
+    names(keywords2)[names(keywords2) == "Var1"] <- "Keyword"
+    keywords2 <- keywords2[rev(order(keywords2$Freq)),]
+    row.names(keywords2) <- NULL
+    keywords2 <- keywords2[keywords2$Keyword %in% filteredKeywords()$Keyword,]
+    keywords2
   })
   
   #Reactive Function that changes the dataframe based on the filter the user picks
@@ -515,84 +525,168 @@ server <- function(input, output) {
   })
   
   output$MoviesPerGenreChart <- renderPlot({
-    df1 <- rawdata[,c("Genres", "set")]
-    df1 <- data.frame(table(df1["Genres"]))
-    colnames(df1) <- c("Genres", "Freq")
-    df1["set"] <- "All"
-    df2 <- data()[,c("Genres", "set")]
-    df2 <- data.frame(table(df2["Genres"]))
-    colnames(df2) <- c("Genres", "Freq")
-    df2["set"] <- "Filtered"
-    df <- rbind(df1, df2)
-    #amount of movies per Genre
-    ggplot(df) +
-      aes(x = Genres, y = Freq, fill = set) +
-      geom_col(position = "dodge") +
-      labs(title="Number of Movies per Genre", caption="source: Genres") +
-      labs(x = "Genre", y = "Count") +
-      theme(axis.text.x = element_text(angle=65, vjust=0.6))
+    if(isFiltered()) {
+      df1 <- rawdata[,c("Genres", "set")]
+      df1 <- data.frame(table(df1["Genres"]))
+      colnames(df1) <- c("Genres", "Freq")
+      df1["set"] <- "All"
+      df2 <- data()[,c("Genres", "set")]
+      df2 <- data.frame(table(df2["Genres"]))
+      colnames(df2) <- c("Genres", "Freq")
+      df2["set"] <- "Filtered"
+      df <- rbind(df1, df2)
+      #amount of movies per Genre
+      ggplot(df) +
+        aes(x = Genres, y = Freq, fill = set) +
+        geom_col(position = "dodge") +
+        labs(title="Number of Movies per Genre", caption="source: Genres") +
+        labs(x = "Genre", y = "Count") +
+        theme(axis.text.x = element_text(angle=65, vjust=0.6))
+    }
+    else {
+      #amount of movies per Genre
+      ggplot(data(), aes(x=factor(data()$Genres))) +
+        geom_bar( width=.5, fill="tomato3") +
+        labs(title="Number of Movies per Genre", caption="source: Genres") +
+        labs(x = "Genre", y = "Count") +
+        theme(axis.text.x = element_text(angle=65, vjust=0.6))
+    }
   })
   
   output$MoviesPerGenreTable = DT::renderDataTable({
-    #table for genre
-    genreTable <- as.data.frame(table(data()$Genres))
-    names(genreTable)[names(genreTable) == "Var1"] <- "Genres"
-    genreTable
+    if(isFiltered()) {
+      df1 <- rawdata[,c("Genres", "set")]
+      df1 <- data.frame(table(df1["Genres"]))
+      colnames(df1) <- c("Genres", "Freq")
+      df1["set"] <- "All"
+      df2 <- data()[,c("Genres", "set")]
+      df2 <- data.frame(table(df2["Genres"]))
+      colnames(df2) <- c("Genres", "Freq")
+      df2["set"] <- "Filtered"
+      df <- rbind(df1, df2)
+    }
+    else {
+      #table for genre
+      genreTable <- as.data.frame(table(data()$Genres))
+      names(genreTable)[names(genreTable) == "Var1"] <- "Genres"
+      genreTable
+    }
   })
   
   output$MoviesPerCertificateChart <- renderPlot({
-    #amount of movies per Certificate
-    ggplot(data(), aes(x=factor(data()$Certificate))) +
-      geom_bar( width=.5, fill="tomato3") +
-      labs(title="Number of Movies per Certificate", caption="source: Certificate") +
-      labs(x = "Certificate", y = "Count") +
-      theme(axis.text.x = element_text(angle=65, vjust=0.6))
+    if(isFiltered()) {
+      df1 <- overallData[,c("Certificate", "set")]
+      df1 <- data.frame(table(df1["Certificate"]))
+      colnames(df1) <- c("Certificate", "Freq")
+      df1["set"] <- "All"
+      df2 <- uniquedata()[,c("Certificate", "set")]
+      df2 <- data.frame(table(df2["Certificate"]))
+      colnames(df2) <- c("Certificate", "Freq")
+      df2["set"] <- "Filtered"
+      df <- rbind(df1, df2)
+      #amount of movies per Certificate
+      ggplot(df) +
+        aes(x = Certificate, y = Freq, fill = set) +
+        geom_col(position = "dodge") +
+        labs(title="Number of Movies per Certificate", caption="source: Certificate") +
+        labs(x = "Certificate", y = "Count") +
+        theme(axis.text.x = element_text(angle=65, vjust=0.6))
+    }
+    else {
+      #amount of movies per Certificate
+      ggplot(data(), aes(x=factor(data()$Certificate))) +
+        geom_bar( width=.5, fill="tomato3") +
+        labs(title="Number of Movies per Certificate", caption="source: Certificate") +
+        labs(x = "Certificate", y = "Count") +
+        theme(axis.text.x = element_text(angle=65, vjust=0.6))
+    }
   })
   
   output$MoviesPerCertificateTable = DT::renderDataTable({
-    #table for movies per Certificate
-    certificateTable <- as.data.frame(table(data()$Certificate))
-    names(certificateTable)[names(certificateTable) == "Var1"] <- "Certificate"
-    certificateTable
+    if(isFiltered()) {
+      df1 <- overallData[,c("Certificate", "set")]
+      df1 <- data.frame(table(df1["Certificate"]))
+      colnames(df1) <- c("Certificate", "Freq")
+      df1["set"] <- "All"
+      df2 <- uniquedata()[,c("Certificate", "set")]
+      df2 <- data.frame(table(df2["Certificate"]))
+      colnames(df2) <- c("Certificate", "Freq")
+      df2["set"] <- "Filtered"
+      df <- rbind(df1, df2)
+      df
+    }
+    else {
+      #table for movies per Certificate
+      certificateTable <- as.data.frame(table(data()$Certificate))
+      names(certificateTable)[names(certificateTable) == "Var1"] <- "Certificate"
+      certificateTable
+    }
   })
   
   output$MovieKeywordsChart <- renderPlot({
-    #plot for the keyword
-    ggplot(filteredKeywords(), aes(x= Keyword, y = factor(Freq))) +
-      geom_bar(position="dodge", stat="identity", width=.5, fill="tomato3") +
-      labs(title="Number of Movies per Keyword", caption="source: Keyword") +
-      labs(x = "Keyword", y = "Count") +
-      theme(axis.text.x = element_text(angle=90, vjust=0.6)) + 
-      coord_flip()
+    if(isFiltered()) {
+      df1 <- filteredKeywords()
+      df1["set"] <- "All"
+      df2 <- extraKeywords()
+      df2["set"] <- "Filtered"
+      df <- rbind(df1, df2)
+      #plot for the keyword
+      ggplot(df) +
+        aes(x = Keyword, y = Freq, fill = set) +
+        geom_col(position = "dodge") +
+        labs(title="Number of Movies per Keyword", caption="source: Keyword") +
+        labs(x = "Keyword", y = "Count") +
+        theme(axis.text.x = element_text(angle=90, vjust=0.6)) + 
+        coord_flip()
+    }
+    else {
+      #plot for the keyword
+      ggplot(filteredKeywords(), aes(x= Keyword, y = factor(Freq))) +
+        geom_bar(position="dodge", stat="identity", width=.5, fill="tomato3") +
+        labs(title="Number of Movies per Keyword", caption="source: Keyword") +
+        labs(x = "Keyword", y = "Count") +
+        theme(axis.text.x = element_text(angle=90, vjust=0.6)) + 
+        coord_flip()
+    }
   })
   
   output$MovieKeywordsTable = DT::renderDataTable({
-    #table for movies per keywords
-    filteredKeywords()
+    if(isFiltered()) {
+      df1 <- filteredKeywords()
+      df1["set"] <- "All"
+      df2 <- extraKeywords()
+      df2["set"] <- "Filtered"
+      df <- rbind(df1, df2)
+      df
+    }
+    else {
+      #table for movies per keywords
+      filteredKeywords()
+    }
   })
   
-  output$RuntimeGenrePlot <- renderPlot({
-    ##shows the min and max Runtime of each Genre
-    ggplot(overallData, aes(x=Genres, y=Running.Time, group = Running.Time)) +
-      geom_point(col="tomato2", size=3) +   # Draw points
-      geom_segment(
-        aes(x=Genres, xend=Genres, y=min(Running.Time), yend=max(Running.Time)), 
-        linetype="dashed", size=0.1
-      ) + 
-      labs(title="Genre & Run Time", caption="Genre & Run Time") +  
-      labs(x = "Genre", y = "Runtime (in minutes)") +
-      coord_flip()
-  })
-  
-  output$RuntimeGenreTable = DT::renderDataTable({
-    #table for runtime/genre
-    runtimeGenresTable <- as.data.frame(table(overallData$Genres,overallData$Running.Time))
-    names(runtimeGenresTable)[names(runtimeGenresTable) == "Var1"] <- "Genres"
-    names(runtimeGenresTable)[names(runtimeGenresTable) == "Var2"] <- "RunTime"
-    runtimeGenresTable <- runtimeGenresTable[rev(order(runtimeGenresTable$Freq)),]
-    rownames(runtimeGenresTable) <- NULL
-    runtimeGenresTable
-  })
+  # output$RuntimeGenrePlot <- renderPlot({
+  #   ##shows the min and max Runtime of each Genre
+  #   ggplot(overallData, aes(x=Genres, y=Running.Time, group = Running.Time)) +
+  #     geom_point(col="tomato2", size=3) +   # Draw points
+  #     geom_segment(
+  #       aes(x=Genres, xend=Genres, y=min(Running.Time), yend=max(Running.Time)), 
+  #       linetype="dashed", size=0.1
+  #     ) + 
+  #     labs(title="Genre & Run Time", caption="Genre & Run Time") +  
+  #     labs(x = "Genre", y = "Runtime (in minutes)") +
+  #     coord_flip()
+  # })
+  # 
+  # output$RuntimeGenreTable = DT::renderDataTable({
+  #   #table for runtime/genre
+  #   runtimeGenresTable <- as.data.frame(table(overallData$Genres,overallData$Running.Time))
+  #   names(runtimeGenresTable)[names(runtimeGenresTable) == "Var1"] <- "Genres"
+  #   names(runtimeGenresTable)[names(runtimeGenresTable) == "Var2"] <- "RunTime"
+  #   runtimeGenresTable <- runtimeGenresTable[rev(order(runtimeGenresTable$Freq)),]
+  #   rownames(runtimeGenresTable) <- NULL
+  #   runtimeGenresTable
+  # })
   
   
 }#end ofserver
